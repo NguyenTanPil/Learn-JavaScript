4c7d31d4c32ce0685b728fe3fb7ae84ee7a28a48
NguyenTanPil <traila1008@gmail.com>
2021-03-07 13:51:03 +0700

    Add couter project

---
+++ counter/app.js
@@ -0,0 +1,19 @@
+let count = 0;
+let btns = document.querySelectorAll('.btn');
+let value = document.querySelector('#value');
+btns.forEach((btn) => {
+	btn.addEventListener('click', (e) => {
+		const styles = e.currentTarget.classList;
+		if(styles.contains('decrease')) {
+			count--;
+		} else if(styles.contains('increase')) {
+			count++;
+		} else count = 0;
+		if(count < 0) {
+			value.style.color = 'red';
+		} else if(count > 0) {
+			value.style.color = 'green';
+		} else value.style.color = '#222';
+		value.textContent = count;
+	})
+});
