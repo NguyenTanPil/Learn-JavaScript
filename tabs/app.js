4355c12ae75cb404ccb9a6f529488a4296ec7e07
NguyenTanPil <traila1008@gmail.com>
2021-03-13 15:39:59 +0700

    Add tabs project

---
+++ tabs/app.js
@@ -0,0 +1,17 @@
+const btns = document.querySelectorAll('.tab-btn');
+const about = document.querySelector('.btn-container');
+const contents = document.querySelectorAll('.content');
+about.addEventListener('click', (e) => {
+	const id = e.target.dataset.id;
+	console.log(id);
+	// remove all active class from other btn
+	btns.forEach((btn) => {
+		btn.classList.remove('active');
+	});
+	contents.forEach((content) => {
+		content.classList.remove('active');
+	});
+	e.target.classList.add('active');
+	const active = document.getElementById(id);
+	active.classList.add('active');
+});
